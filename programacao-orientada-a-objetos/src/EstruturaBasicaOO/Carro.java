0df8363976801b36715d618812c184ca8da3395e
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-04 22:25:13 -0300

    Aula 4 de Estrutura Básica da OO - Objeto e Mensagem

--- programacao-orientada-a-objetos/src/EstruturaBasicaOO/Carro.java
+++ programacao-orientada-a-objetos/src/EstruturaBasicaOO/Carro.java
@@ -1,14 +1,22 @@
 /* Aula sobre estrutura básica da OO */
 
 /*Crie a classe "Carro"   - Classe - Parte 1*/
+
 /*Evolua o exercício 1 e defina 3 atributos para
   sua classe "Carro": cor, modelo e capacidade do 
   tanque  - Atributos - Parte 2*/
+
 /*Evolua o conceito do exercício 2 e defina 1 método
   para calcular o valor total para encher o tanque.
   Este deve receber como parâmetro o valor da gasolina.
   Faça também duas sobrecargas do construtor - Método -
   Parte 3*/
+
+  /*Evolua o conceito do exercício 3 criando objetos da
+    classe "Carro". Use os métodos set/get, quando aplicáveis,
+    para definir os valores dos atributos e exibir estes valores
+    "get". Passe também uma mensagem para o cálculo do total para
+    encher o tanque. - Objeto e Mensagem - Parte 4  */
 
 package EstruturaBasicaOO;
 
