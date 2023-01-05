ab45b6f3bdd8f24fe0dc82daf9872993790cd885
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-04 21:38:58 -0300

    Aula 2 de Estrutura Básica da OO - Atributos

--- programacao-orientada-a-objetos/src/EstruturaBasicaOO/Carro.java
+++ programacao-orientada-a-objetos/src/EstruturaBasicaOO/Carro.java
@@ -1,9 +1,16 @@
-/* Aula 1 sobre estrutura básica da OO - Classes */
+/* Aula sobre estrutura básica da OO */
 
-/*Crie a classe "Carro" */
+/*Crie a classe "Carro"   - Classe - Parte 1*/
+/*Evolua o exercício 1 e defina 3 atributos para
+  sua classe "Carro": cor, modelo e capacidade do 
+  tanque  - Atributos - Parte 2*/
 
 package EstruturaBasicaOO;
 
 public class Carro {
+
+    String cor;
+    String modelo;
+    int capacidadeTanque;
     
 }
