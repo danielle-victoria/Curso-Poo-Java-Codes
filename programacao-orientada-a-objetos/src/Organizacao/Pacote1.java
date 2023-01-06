77b5e93758a1e1e8505beee795129dcee6007354
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-05 23:17:59 -0300

    Aula 1-Organização de pacotes e visibilidade - Ex1

---
+++ programacao-orientada-a-objetos/src/Organizacao/Pacote1.java
@@ -0,0 +1,11 @@
+/*A Organização de Pacotes e Visibilidade */
+package Organizacao;
+
+//import AsRelacoes.*;
+import AsRelacoes.Heranca.RodarAplicacao;;
+
+public class Pacote1 {
+
+    RodarAplicacao outroPacote;
+    
+}
