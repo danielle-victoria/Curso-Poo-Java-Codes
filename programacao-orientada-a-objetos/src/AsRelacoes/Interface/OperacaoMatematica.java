1959038a6b7b9478fb3dc07e99c6ea4c3bef39ea
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-05 23:07:31 -0300

    Aula 5 de Herança, associação e interface - Ex5

---
+++ programacao-orientada-a-objetos/src/AsRelacoes/Interface/OperacaoMatematica.java
@@ -0,0 +1,21 @@
+/*As relações: Heranças, Associação e Interface */
+
+/*Apenas para praticar, crie uma interface chamada
+ "OperacaoMatematica". Crie também 4 métodos das 
+ operações básicas: soma, subtração, multiplicação
+ e divisão. - Interface*/
+
+package AsRelacoes.Interface;
+
+interface OperacaoMatematica {
+
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+
+    
+}
