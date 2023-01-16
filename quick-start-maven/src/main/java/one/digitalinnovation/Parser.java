091eabdf4b524a74977f59754508db53802cf913
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-15 22:09:23 -0300

    POM - Tipos de dependências

---
+++ quick-start-maven/src/main/java/one/digitalinnovation/Parser.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation;
+
+/**
+ * Hello world!
+ *
+ */
+public class Parser 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    }
+
+    public void message(){
+        System.out.println("Component");
+    }
+}
