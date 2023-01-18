ab2423043921879cb3b1022b734bdabcf21ce573
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-18 01:05:20 -0300

    Testes Com Mockito - Mockando Métodos Estáticos

---
+++ mockito-exemplos/src/main/java/one/digitalinnovation/GeradorDeNumeros.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation;
+
+import java.util.List;
+import java.util.SplittableRandom;
+import java.util.stream.Collectors;
+
+public final class GeradorDeNumeros {
+
+    private static SplittableRandom random = new SplittableRandom();
+
+    private GeradorDeNumeros(){
+    }
+
+    public static List<Integer> geraNumerosAleatorios(int tamanhoLista) {
+        return random.ints().boxed().limit(tamanhoLista).collect(Collectors.toList());
+    }
+
+    public static List<Integer> geraNumerosAleatorios() {
+        return random.ints().boxed().limit(random.nextInt(10)).collect(Collectors.toList());
+    }
+}
+
+
+//OBS.: EM pom.xml --><dependencies> --> Trocar core por inline para testar uma classe estática
